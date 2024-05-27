d3ddd8d733d8ad3bcb64852178187764a391e16c
Spectrum-Edge <91475504+Declan-S@users.noreply.github.com>
2024-05-27 12:54:57 +0100

    checkpoint

--- IRP Project - Noneuclidean Portals/Assets/NonEuclideanPortals/Scripts/PortalPair.cs
+++ IRP Project - Noneuclidean Portals/Assets/NonEuclideanPortals/Scripts/PortalPair.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class PortalPair : MonoBehaviour
 {
+    [SerializeField] private LayerMask transportLayers;
     [SerializeField] private float LODDistance = 1024f;
     [SerializeField] private Camera mainCam;
 
